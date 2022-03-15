d13aa93e4ce9868116242ff6cbeb20f99ae86137
hugo miranda <hugo.miranda.souza@gmail.com>
2022-03-14 22:43:55 -0300

    seed db if has no data stored

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { createConnection, getManager } from "typeorm";
+import { createConnection, getManager, getRepository } from "typeorm";
 import { Client } from "./Client.entity";
 
 async function main() {
@@ -7,7 +7,24 @@ async function main() {
   console.log("connected!");
 
   const manager = getManager();
+  const clientRepository = getRepository(Client);
 
+  const count = await clientRepository.count();
+  if (count === 0) {
+    console.log("creating clients");
+    const clients = Array(1000)
+      .fill(null)
+      .map((_, index) =>
+        clientRepository.create({
+          name: `Client ${index}`,
+          email: `Email ${index}`,
+        })
+      );
+    console.log("saving new clients");
+    await clientRepository.save(clients);
+  }
+
+  console.log("getting clients from stream");
   const stream = await manager.createQueryBuilder(Client, "c").stream();
 
   stream.on("data", (client) => {
